bbe44bbbd7cf40100fefaa6d140b44f979c7c4ba
wenchi <b99901053@gmail.com>
2018-02-11 00:34:11 -0800

    modify

--- untar.sh
+++ untar.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-for file in *.tar.gz
+for year in 2015 2016 2017
 do
-	mkdir $(echo $file | sed "s/\.tar.gz//")
-	mv $file ./$(echo $file | sed "s/\.tar.gz//")
-	cd ./$(echo $file | sed "s/\.tar.gz//")
-	tar zxvf *
-	cd ..
-	#remove=$file
-	#echo $remove
-done
+	cd $year
+	for file in *.tar.gz
+	do
+		mkdir $(echo $file | sed "s/\.tar.gz//")
+		mv $file ./$(echo $file | sed "s/\.tar.gz//")
+		cd ./$(echo $file | sed "s/\.tar.gz//")
+		tar zxvf *
+		cd ..
+		#remove=$file
+		#echo $remove
+	done
 
-rm ./*/*.tar.gz
+	rm ./*/*.tar.gz
+	cd ..
+done
